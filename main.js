15ad0d29e597341b252e2212223b285fcb35e2dc
Kin <riennsuu@gmail.com>
2024-01-07 13:00:10 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 const kv = await Deno.openKv();
 
 Deno.serve(async (request) => {
-	const lastVisited = (await kv.get("lastVisited")).value ?? null;
-	const visits = (await kv.get("visits")).value ?? 0;
+	const lastVisited = (await kv.get(["lastVisited"])).value ?? null;
+	const visits = (await kv.get(["visits"])).value ?? 0;
 	
 	const atomic = kv.atomic();
 	
 	const newLastVisited = new Date();
-	atmoic.set("lastVisited", newLastVisited);
+	atmoic.set(["lastVisited"], newLastVisited);
 	const newVisits = visits + 1;
-	atomic.set("visits", newVisits);
+	atomic.set(["visits"], newVisits);
 
 	await atomic.commit();
 	return new Response(`Last Visit: ${newLastVisited}\nVisits: ${newVisits}`);
